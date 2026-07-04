48fc484a4bd94d74843343a551737b3ba9adcc34
RifatKhanSakil <your-email@example.com>
2026-07-05 00:57:30 +0600

    added JavaScript

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function welcomeMessage() {
+    alert("Welcome to Khan's Pharma! We are happy to serve you.");
+}
+
+function showOffer() {
+    document.getElementById("offer").innerHTML =
+    "Today's Special Offer: Get 20% OFF on all Vitamins!";
+}
+
+function contactMessage() {
+    alert("Thank you! Your message has been sent successfully.");
+}
+
+function thankYou() {
+    alert("Thank you for visiting Khan's Pharma!");
+}
